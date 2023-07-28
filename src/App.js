69de83d9bf2a3ef991f6b7963eef2e5034dc84cd
nartrix <dimitri.danielo@laposte.net>
2023-07-28 19:14:27 +0200

    add and build kpi chart

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navigation from "./components/navigation";
 import WeightChart from "./components/weight-chart";
 import DurationChart from "./components/duration-chart";
 import RadarChart from "./components/radar-chart";
+import KpiChart from "./components/kpi-chart";
 
 function App() {
   return (
@@ -30,60 +31,100 @@ function App() {
           <p style={{ fontSize: "18px", marginBottom: "47px" }}>
             Félicitation ! Vous avez explosé vos objectifs hier 👏
           </p>
-          <div
-            className="weight-chart"
-            style={{
-              width: "835px",
-              height: "320px",
-              backgroundColor: "#FBFBFB",
-              borderRadius: "5px",
-              marginBottom: "30px",
-            }}
-          >
-            <p style={{ padding: "24px 0px 0 32px", fontSize: "18px" }}>
-              Activité quotidienne
-            </p>
-            <WeightChart />
-          </div>
-          <div className="card-chart" style={{ display: "flex", gap: "36px" }}>
+          <div className="charts">
             <div
-              className="duration-chart"
+              className="weight-chart"
               style={{
-                width: "258px",
-                height: "263px",
-                backgroundColor: "#FF0000",
-                boxShadow: "0px 2px 4px 0px rgba(0, 0, 0, 0.02)",
+                width: "835px",
+                height: "320px",
+                backgroundColor: "#FBFBFB",
                 borderRadius: "5px",
-                overflow: "hidden",
+                marginBottom: "30px",
               }}
             >
-              <p
+              <p style={{ padding: "24px 0px 0 32px", fontSize: "18px" }}>
+                Activité quotidienne
+              </p>
+              <WeightChart />
+            </div>
+            <div
+              className="card-chart"
+              style={{ display: "flex", gap: "36px" }}
+            >
+              <div
+                className="duration-chart"
                 style={{
-                  width: "147px",
-                  color: "#FFF",
-                  fontSize: "15px",
-                  opacity: "0.5",
-                  marginLeft: "29px",
-                  marginTop: "24px",
+                  width: "258px",
+                  height: "263px",
+                  backgroundColor: "#FF0000",
+                  boxShadow: "0px 2px 4px 0px rgba(0, 0, 0, 0.02)",
+                  borderRadius: "5px",
+                  overflow: "hidden",
                 }}
               >
-                Durée moyenne des sessions
-              </p>
-              <DurationChart />
+                <p
+                  style={{
+                    width: "147px",
+                    color: "#FFF",
+                    fontSize: "15px",
+                    opacity: "0.5",
+                    marginLeft: "29px",
+                    marginTop: "24px",
+                  }}
+                >
+                  Durée moyenne des sessions
+                </p>
+                <DurationChart />
+              </div>
+              <div
+                className="radar-chart"
+                style={{
+                  width: "258px",
+                  height: "263px",
+                  backgroundColor: "#282D30",
+                  borderRadius: "5px",
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <RadarChart />
+              </div>
+              <div
+                className="Kpi-chart"
+                style={{
+                  width: "258px",
+                  height: "263px",
+                  backgroundColor: "#FBFBFB",
+                  borderRadius: "5px",
+                  position: "relative",
+                }}
+              >
+                <p
+                  style={{
+                    margin: "20px 0px 0px 34px",
+                    fontWeight: "bold",
+                    fontSize: "15px",
+                    color: "#20253A",
+                  }}
+                >
+                  Score
+                </p>
+                <KpiChart />
+                <p
+                  style={{
+                    width: "95px",
+                    position: "absolute",
+                    top: "52%",
+                    left: "39%",
+                    color: "#74798C",
+                    fontSize: "16px",
+                  }}
+                >
+                  de votre objectifs
+                </p>
+              </div>
             </div>
-            <div
-              className="radar-chart"
-              style={{
-                width: "258px",
-                height: "263px",
-                backgroundColor: "#282D30",
-                borderRadius: "5px",
-                display: "flex",
-                justifyContent: "center",
-              }}
-            >
-              <RadarChart />
-            </div>
+            <div className="cards"></div>
           </div>
         </div>
       </main>
--- src/components/kpi-chart/index.jsx
+++ src/components/kpi-chart/index.jsx
@@ -1,9 +1,73 @@
+import {
+  RadialBarChart,
+  RadialBar,
+  Legend,
+  PolarAngleAxis,
+  ResponsiveContainer,
+} from "recharts";
+
 function kpiChart() {
+  const data = [
+    {
+      name: "12%",
+      uv: 12,
+      pv: 2400,
+      fill: "#E60000",
+    },
+  ];
+
+  const renderColorLegendText = (value) => {
     return (
-        <div>
-            
-        </div>
-    )
+      <span
+        style={{
+          width: "95px",
+          color: "#282D30",
+          fontSize: "26px",
+          fontWeight: "bold",
+        }}
+      >
+        {value}
+      </span>
+    );
+  };
+
+  return (
+    <>
+      <ResponsiveContainer width="100%" height="75%">
+        <RadialBarChart
+          cx="50%"
+          cy="50%"
+          innerRadius="100%"
+          outerRadius="85%"
+          barSize={16}
+          data={data}
+          startAngle={190}
+          endAngle={-190}
+        >
+          <PolarAngleAxis
+            type="number"
+            domain={[0, 100]}
+            angleAxisId={0}
+            tick={false}
+          />
+          <RadialBar
+            dataKey="uv"
+            background
+            clockWise={true}
+            cornerRadius={30 / 2}
+          />
+          <Legend
+            formatter={renderColorLegendText}
+            iconSize={0}
+            layout="vertical"
+            verticalAlign="middle"
+            stroke="#020203"
+          />
+          <text>votre objectif</text>
+        </RadialBarChart>
+      </ResponsiveContainer>
+    </>
+  );
 }
 
 export default kpiChart;
